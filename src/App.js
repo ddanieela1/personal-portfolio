8f65f9de103d5cc097179fef134a0f45aa9eba79
ddanieela1 <vdaniela.gv@gmail.com>
2023-01-25 13:36:09 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'NavBar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1>App</h1>
+        <NavBar/>
+  
+    </div>
+  );
+}
+
+export default App;
